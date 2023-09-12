a0fa5f65232ba5da3f7ecc8ccfa1897b32042c05
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2023-09-12 19:38:55 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -133,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
